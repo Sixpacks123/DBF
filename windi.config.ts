9a2c7cf9796516b29dae3f464254645e33dfff3e
Heurtault Aubin <aub.heurtault@gmail.com>
2023-03-01 18:22:53 +0100

    config windiCss

--- windi.config.ts
+++ windi.config.ts
@@ -1,5 +1,18 @@
 import { defineConfig } from 'windicss/helpers'
 
+function range(size : number, startAt = 1) {
+    return Array.from(Array(size).keys()).map(i => i + startAt)
+}
+
 export default defineConfig({
-    /* configurations... */
+    attributify: false,
+    safelist: [...Array(5).keys()]
+        .filter((key) => key > 0)
+        .map(
+            (key) =>
+                `grid-cols-${key} lg:grid-cols-${key} xl:grid-cols-${key} md:grid-cols-${key} sm:grid-cols-${key}`
+        ),
+    theme: {
+
+    },
 })
