af3a722a59ec1c77161db793050796b482f9b277
Heurtault Aubin <aub.heurtault@gmail.com>
2023-03-02 21:21:19 +0100

    Create BaseButton

---
+++ components/Base/BaseButton.vue
@@ -0,0 +1,12 @@
+<template>
+  <NuxtLink class="bg-experimental rounded-full">
+    <span class="px-3 py-2">{{props.title}}</span>
+  </NuxtLink>
+</template>
+<script setup lang="ts">
+const props =defineProps({
+  title:{
+    type:String
+  },
+})
+</script>
