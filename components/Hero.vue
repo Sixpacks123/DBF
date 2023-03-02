129531fb50edd345a8e1101596824296891f2343
Heurtault Aubin <aub.heurtault@gmail.com>
2023-03-02 22:06:36 +0100

    Hero Header

---
+++ components/Hero.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg-gray-900 h-screen w-screen text-white py-20 flex items-end	">
+    <div class="container mx-auto mb-2 ">
+      <h1 class="text-7xl text-Helveticaneue font-bold">Studio expérimental <br> & design gaphique</h1>
+      <div class="flex flew-row w-full justify-between flex-end pb-20 pt-20">
+        <img class="rounded h-30 w-60" src="https://picsum.photos/200">
+        <img class="rounded h-30 w-60" src="https://picsum.photos/200">
+        <img class="rounded h-30 w-60" src="https://picsum.photos/200">
+        <img class="rounded h-30 w-60" src="https://picsum.photos/200">
+        <img class="rounded h-30 w-60" src="https://picsum.photos/200">
+        <img class="rounded h-30 w-60" src="https://picsum.photos/200">
+      </div>
+    </div>
+    <div class="absolute bottom-0 w-full flex flex-row justify-center p-2 bg-experimental">
+      test
+    </div>
+  </div>
+</template>
+<style scoped>
+</style>
