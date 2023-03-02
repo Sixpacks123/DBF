69368e0e3aeb485253ae2ff1abcae8c8a1cc35a7
Heurtault Aubin <aub.heurtault@gmail.com>
2023-03-02 22:06:44 +0100

    Main Pages

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Hero />
+    <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+      <h2 class="text-3xl font-bold leading-tight text-gray-900">
+        Welcome to my Nuxt.js App
+      </h2>
+      <p class="mt-4 text-gray-600">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis commodo mi et justo faucibus, nec laoreet felis vulputate. Suspendisse potenti. In sed eros sit amet urna molestie bibendum a auctor nunc.
+      </p>
+    </div>
+  </div>
+</template>
